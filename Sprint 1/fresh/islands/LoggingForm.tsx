2f9a4a6af60af6dee954b53700e7e7a5b3e62246
Daniel Blackin <118198990+danielblackin@users.noreply.github.com>
2024-11-01 13:05:19 -0400

    did constants

--- Sprint 1/fresh/islands/LoggingForm.tsx
+++ Sprint 1/fresh/islands/LoggingForm.tsx
@@ -7,9 +7,17 @@ export default function LoggingForm() {
 		const log = event.target as HTMLFormElement;
 
 		//Form Values go here.
+
+		const pain_level = form.querySelector('[name="painLevel"]') as HTMLInputElement.value;
+		const quality_of_life = form.querySelector('[name="qualityOfLife"]') as HTMLInputElement.value;
+		const satisfaction = form.querySelector('[name="satisfaction"]') as HTMLInputElement.value;
+		const pain_level = form.querySelector('[name="painLevel"]') as HTMLInputElement.value;
+		const additional_observations = form.querySelector('[name="additionalObservations"]') as HTMLInputElement.value;
+		
 	};
 
 	return (
+		<form style="
 		
 	)
 }
