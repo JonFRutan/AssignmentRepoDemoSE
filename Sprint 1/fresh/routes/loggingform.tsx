bcade8a89e7b5b9db0928b519dec42f9ef658ac2
JonFRutan <jonfrutan@users.noreply.github.com>
2024-11-04 15:06:05 -0500

    updates

--- Sprint 1/fresh/routes/loggingform.tsx
+++ Sprint 1/fresh/routes/loggingform.tsx
@@ -9,7 +9,7 @@ export default function loggingform() {
 				<title>Log Form</title>
 			</Head>
 			<div style="width: 100%; height: 8rem; background-color: #6d76ff; display: flex; align-items: center; justify-content: center;">
-				<h1 style="color: white; font-size: 2.5 rem; font-weight: bold;">Log Form</h1>
+				<h1 style="color: white; font-size: 2.5rem; font-weight: bold;">Log Form</h1>
 			</div>
 			<LoggingForm />
 		</div>
